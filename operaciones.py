cf917c44ce95303e04c0f9e6bf16c7b51c0269d3
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-12-19 23:02:22 +0100

    division entre cero cubierta

--- operaciones.py
+++ operaciones.py
@@ -8,4 +8,8 @@ def producto(a, b):
     return a*b
 
 def division(a,b):
-    return a/b
+    try:
+        return a/b
+    except ZeroDivisionError:
+        print("No se puede dividir por cero")
+        return None
