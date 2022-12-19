5430902a0a11f3a7a2437bf8a4690484b8f79a2e
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-12-19 23:06:32 +0100

    creo que he solucionado el error

--- operaciones.py
+++ operaciones.py
@@ -1,23 +1,23 @@
 def suma(a, b):
-    if(type(a)==str and type(b)==str):
+    if(type(a)==str or type(b)==str):
         print("No se puede sumar una cadena de string")
     else:
         return a + b
 
 def resta(a, b):
-    if(type(a)==str and type(b)==str):
+    if(type(a)==str or type(b)==str):
         print("No se puede restar una cadena de string")
     else:
         return a - b
 
 def producto(a, b):
-    if(type(a)==str and type(b)==str):
+    if(type(a)==str or type(b)==str):
         print("No se puede multiplicar una cadena de string")
     else:
         return a*b
 
 def division(a,b):
-    if(type(a)==str and type(b)==str):
+    if(type(a)==str or type(b)==str):
         print("No se puede dividir una cadena de string")
     else:
         try:
