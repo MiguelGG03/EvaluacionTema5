125bc797b0c715c7c1cd4881fabcfc4fa7d0f448
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-12-20 10:30:10 +0100

    init cambiado

--- gestor.py
+++ gestor.py
@@ -1,10 +1,30 @@
 class Personaje:
-    def __init__(self,nombre,vida:int>0,daño:int>0,defensa:int>0,alcanze:int>0) -> None:
-        self.nomnbre=nombre
-        self.vida=vida
-        self.daño=daño
-        self.defensa=defensa
+    def __init__(self,nombre:str,vida:int,daño:int,defensa:int,alcanze:int):
+        if(type(nombre)!=str):
+            raise TypeError('El nombre debe ser un string')
+        else:
+            self.nombre=nombre
+        if(type(vida)!=int):
+            raise TypeError('La vida debe ser un entero')
+        else:
+            self.vida=vida
+        if(type(daño)!=int):
+            raise TypeError('El daño debe ser un entero')
+        else:
+            self.daño=daño
+        if(type(defensa)!=int):
+            raise TypeError('La defensa debe ser un entero')
+        else:
+            self.defensa=defensa
+        if(type(alcanze)!=int):
+            raise TypeError('El alcanze debe ser un entero')
+        else:
+            self.alcanze=alcanze
 
 
 class Gestor:
     pass
+
+def main_gestor():
+    p=Personaje('Juan',100,10,5,0)
+    print(p)
