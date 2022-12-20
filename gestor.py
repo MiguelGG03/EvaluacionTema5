287074996b430dbaf1ea094ad8e5697c2471f112
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-12-20 10:31:39 +0100

    init UWU

--- gestor.py
+++ gestor.py
@@ -6,18 +6,26 @@ class Personaje:
             self.nombre=nombre
         if(type(vida)!=int):
             raise TypeError('La vida debe ser un entero')
+        elif(vida<=0):
+            raise ValueError('La vida debe ser mayor que 0')
         else:
             self.vida=vida
         if(type(daño)!=int):
             raise TypeError('El daño debe ser un entero')
+        elif(daño<=0):
+            raise ValueError('El daño debe ser mayor que 0')
         else:
             self.daño=daño
         if(type(defensa)!=int):
             raise TypeError('La defensa debe ser un entero')
+        elif(defensa<=0):
+            raise ValueError('La defensa debe ser mayor que 0')
         else:
             self.defensa=defensa
         if(type(alcanze)!=int):
             raise TypeError('El alcanze debe ser un entero')
+        elif(alcanze<=0):
+            raise ValueError('El alcanze debe ser mayor que 0')
         else:
             self.alcanze=alcanze
 
