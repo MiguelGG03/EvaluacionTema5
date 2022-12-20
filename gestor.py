b08afa691dc743569ad41ea89155caabfcd96e8a
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-12-20 10:26:52 +0100

    creo que el init de presonajes creado

--- gestor.py
+++ gestor.py
@@ -1,5 +1,9 @@
 class Personaje:
-    pass
+    def __init__(self,nombre,vida:int>0,daño:int>0,defensa:int>0,alcanze:int>0) -> None:
+        self.nomnbre=nombre
+        self.vida=vida
+        self.daño=daño
+        self.defensa=defensa
 
 
 class Gestor:
