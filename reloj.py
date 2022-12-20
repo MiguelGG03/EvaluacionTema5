e1b9a8952903d64028fbb8d5ddb2369a34c9fe6e
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-12-20 11:20:26 +0100

    ya funciona creo

--- reloj.py
+++ reloj.py
@@ -1,8 +1,18 @@
 import os
 import time
 
+
+
 def main_reloj():
-    
+    print('Presione Enter para parar del reloj')
+    print('En cinco segundos se iniciara el reloj')
+    time.sleep(5)
+    while True:
+        os.system('cls')
+        print(time.strftime('%H:%M:%S'))
+        time.sleep(1)
+        if input() == '':
+            break
 
 
 if __name__ == '__main__':
