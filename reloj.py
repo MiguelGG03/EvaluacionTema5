2d70c452748a7a30944c3aca3c9f1931aaac5e06
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-12-20 17:24:48 +0100

    faltaba el else cotinue

--- reloj.py
+++ reloj.py
@@ -7,13 +7,16 @@ def main_reloj():
     print('Presione Enter para parar el reloj')
     print('En cinco segundos se iniciara el reloj')
     time.sleep(5)
+    
     while True:
         os.system('cls')
         print(time.strftime('%H:%M:%S'))
         time.sleep(1)
-        if input() == '':
+        if input()=='':
             break
+        else:
+            continue
 
 
-if __name__ == '__main__':
+if __name__=='__main__':
     main_reloj()
